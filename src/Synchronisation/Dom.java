95418d27bb0bf339737e2bde3a228c7ce0c14fbf
aymeric <aymericbride@yahoo.fr>
2013-10-02 18:11:11 +0200

    ajouter et supprimer un utilisateur

--- src/Synchronisation/Dom.java
+++ src/Synchronisation/Dom.java
@@ -1,5 +1,55 @@
 package Synchronisation;
 
-public class Dom {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.jdom2.Element;
+
+public class Dom {
+	
+	private Element racine= new Element("utilisateurs");
+
+	public void ajoutUtilisateur(Utilisateur u)
+	{
+		Element utilisateur= new Element("utilisateur");
+		racine.addContent(utilisateur);
+		
+		Element id= new Element("id");
+		utilisateur.addContent(id);
+		id.setText(u.id);
+		
+		Element nom= new Element("nom");
+		utilisateur.addContent(nom);
+		nom.setText(u.nom);
+		
+		Element prenom= new Element("prenom");
+		utilisateur.addContent(prenom);
+		prenom.setText(u.prenom);
+		
+		Element mdp= new Element("mdp");
+		utilisateur.addContent(mdp);
+		mdp.setText(u.mdp);
+	}
+	
+	public void supprimerUtilisateur(Utilisateur u)
+	{
+		int i=0;
+		boolean b= false;
+		
+		List<Element> listeUtilisateurs= racine.getChildren("utilisateur");
+
+		Iterator<Element> it = listeUtilisateurs.iterator();
+		
+		while(!b)
+		{
+			Element courant = (Element)it.next();
+			if(!u.id.equals(courant.getChild("id").getText()))
+				i++;
+			else
+				b= true;
+		}
+		
+		racine.removeContent(i);
+	}
 }
